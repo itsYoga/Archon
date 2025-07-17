88105c0235ad3b6e5a63de36d63b16ee4f274dda
YuJiaLiang0702 <ch993115@gmail.com>
2025-07-18 02:13:27 +0800

    Create IdentityRegistry.sol

---
+++ contracts/IdentityRegistry.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract IdentityRegistry is AccessControl {
+    bytes32 public constant KYC_ADMIN_ROLE = keccak256("KYC_ADMIN_ROLE");
+
+    mapping(address => bool) private _isVerified;
+
+    event IdentityRegistered(address indexed user, address indexed admin);
+    event IdentityRevoked(address indexed user, address indexed admin);
+
+    constructor(address admin) {
+        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+        _setupRole(KYC_ADMIN_ROLE, admin);
+    }
+
+    function registerIdentity(address user) external onlyRole(KYC_ADMIN_ROLE) {
+        _isVerified[user] = true;
+        emit IdentityRegistered(user, msg.sender);
+    }
+
+    function revokeIdentity(address user) external onlyRole(KYC_ADMIN_ROLE) {
+        _isVerified[user] = false;
+        emit IdentityRevoked(user, msg.sender);
+    }
+
+    function isVerified(address user) external view returns (bool) {
+        return _isVerified[user];
+    }
+} 
